6332a8713439fc34b5fd164d04dce81fc74fba63
hfangjian <357987103@qq.com>
2017-07-11 09:11:07 +0800

    模板工程更新

---
+++ src/main/java/com/ctg/controller/portal/CartController.java
@@ -0,0 +1,58 @@
+package com.ctg.controller.portal;
+
+import com.ctg.common.Const;
+import com.ctg.common.ResponseCode;
+import com.ctg.common.ServerResponse;
+import com.ctg.pojo.User;
+import com.ctg.service.ICartService;
+import com.ctg.vo.CartVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by hfangjian
+ */
+@Controller
+@RequestMapping("/cart/")
+public class CartController {
+
+    @Autowired
+    private ICartService iCartService;
+
+    @RequestMapping("add.do")
+    @ResponseBody
+    public ServerResponse<CartVo> add(HttpSession session, Integer count, Integer productId){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iCartService.add(user.getId(),productId,count);
+    }
+
+    @RequestMapping("update.do")
+    @ResponseBody
+    public ServerResponse<CartVo> update(HttpSession session, Integer count, Integer productId){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iCartService.update(user.getId(),productId,count);
+    }
+
+    @RequestMapping("delete_product.do")
+    @ResponseBody
+    public ServerResponse<CartVo> deleteProduct(HttpSession session,String productIds){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iCartService.deleteProduct(user.getId(),productIds);
+    }
+
+
+
+}
